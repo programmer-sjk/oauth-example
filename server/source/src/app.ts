6f2186bfe66af8f0b3d8aaab484608496b2699db
sjk5766 <wjdrnr6662@gmail.com>
2020-07-08 14:04:06 +0900

    body-parser 추가

--- server/source/src/app.ts
+++ server/source/src/app.ts
@@ -1,10 +1,13 @@
 
 import express, { Request, Response } from 'express';
+import bodyParser from "body-parser";
 import cors from "cors";
 import { getAccount } from './models/account';
+import { Account } from './interfaces/account';
 
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 
 const port: number = 3000
 app.listen(port, () => console.log('Example app listeningg'))
@@ -14,7 +17,14 @@ app.get('/', (request: Request, response: Response) => {
 });
 
 app.post('/login', (request: Request, response: Response) => {
-    getAccount((err: Error, data: object) => {
-        response.send(data);
+    const id: string = request.body.id;
+    const password: string = request.body.password;
+    
+    getAccount((err: Error, data: Account) => {
+        if(id === data.id && password === data.password) {
+            response.send(data);
+        }
+        else
+            response.send('error');
     })    
 });
