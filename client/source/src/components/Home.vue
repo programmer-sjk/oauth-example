f1fe557fbe5b5309f767b71fcb775e2d7944dc82
sjk5766 <wjdrnr6662@gmail.com>
2020-07-13 21:02:49 +0900

    vue-router를 적용하여 login 페이지와 login 성공 시 페이지 제공

---
+++ client/source/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Login에 성공했습니다. 축하드려요!
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Home',
+    props: [],
+    data: function() {
+        return {
+            oauth_way: 0,
+        }
+    }
+}
+</script>
--- client/source/src/main.js
+++ client/source/src/main.js
@@ -1,11 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueCookies from 'vue-cookies'
+import VueRouter from 'vue-router'
+
+import Home from './components/Home'
+import Login from './components/Login'
 
 Vue.config.productionTip = false
 Vue.prototype.$serverUrl = 'http://localhost:3000';
 Vue.use(VueCookies)
+Vue.use(VueRouter)
+  
+const router = new VueRouter({
+    routes: [
+        { path: '/home', component: Home },
+        { path: '/', component: Login },
+    ],
+    mode: 'history'
+})
 
 new Vue({
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
