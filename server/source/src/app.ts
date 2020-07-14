8a73a9f1635bbd4d9b0a6b5f9513b96b414f5942
sjk5766 <wjdrnr6662@gmail.com>
2020-07-14 13:58:29 +0900

    google oauth 서버 로직 추가

--- server/source/src/app.ts
+++ server/source/src/app.ts
@@ -1,26 +1,51 @@
 
 import express, { Request, Response, NextFunction } from 'express';
 import bodyParser from "body-parser";
+import session from 'express-session';
 import cors from "cors";
+import jwt from 'jsonwebtoken'
+import passport from 'passport';
+import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { getAccount } from './models/account';
 import { Account } from './interfaces/account';
-import jwt from 'jsonwebtoken'
+import { config } from '../config'
 
-const allowRoutes = ['/login', '/token']
+passport.use(new Strategy({
+    clientID: config.client_ID,
+    clientSecret: config.client_secret, 
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(accessToken, refreshToken, profile)
+    return cb(undefined, profile);
+  }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user); 
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user); 
+});
+
+const allowRoutes = ['/login', '/token', '/login/google', '/auth/google/callback']
 const refreshTokens: { [key: string]: string; } = {}
 const app = express();
 app.use(cors({exposedHeaders: 'Authorization'}));
 app.use(bodyParser.json());
-
-const port: number = 3000
-app.listen(port, () => console.log('Example app listeningg'))
-
-
-
+app.use(session({
+    secret: 'secret',
+    cookie: { maxAge: 60 },
+    resave: true,
+    saveUninitialized: false
+}));
+app.use(passport.initialize()); 
+app.use(passport.session());
 app.use((request: Request, response: Response, next: NextFunction) => {
     if(allowRoutes.includes(request.path))
         return next();
-
+    
     const bearer = request?.headers["authorization"]?.split(" ");
     const token = bearer?.[1];
     
@@ -33,8 +58,19 @@ app.use((request: Request, response: Response, next: NextFunction) => {
     response.status(401).json({authorized: false});
 })
 
-app.get('/', (request: Request, response: Response) => {
-    return response.send("you can access / route"); 
+const port: number = 3000
+app.listen(port, () => console.log('Example app listeningg'))
+
+app.get('/login/google', passport.authenticate('google', { scope: ['profile', 'email', 'openid'] }));
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('http://localhost/home');
+});
+
+app.get('/user', (request: Request, response: Response) => {
+    getAccount((err: Error, data: Account) => {
+        response.send(data.id)
+    })
 });
 
 app.post('/login', (request: Request, response: Response) => {
@@ -47,7 +83,7 @@ app.post('/login', (request: Request, response: Response) => {
             const token = jwt.sign(tokenContent, 'secret', { expiresIn: 30 });
             const refreshToken = jwt.sign(tokenContent, 'secret', { expiresIn: 60 * 60 * 24 });
             refreshTokens[refreshToken] = data.id;
-
+            
             response.set('Authorization', 'Bearer ' + token);
             return response.send({refreshToken: refreshToken});
         } 
@@ -68,3 +104,4 @@ app.post('/token', (request: Request, response: Response) => {
         
     return response.status(401).json({authorized: false}); 
 });
+
