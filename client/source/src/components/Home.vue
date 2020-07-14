c364fadb27d32a66661a673edbf0c7967e117057
sjk5766 <wjdrnr6662@gmail.com>
2020-07-14 13:57:47 +0900

    home에서 server로 데이터를 가져오는 버튼 및 display 로직 추가

--- client/source/src/components/Home.vue
+++ client/source/src/components/Home.vue
@@ -1,17 +1,39 @@
 <template>
     <div>
-        Login에 성공했습니다. 축하드려요!
+        <div>
+            Login에 성공했습니다. 축하드려요!
+        </div>
+        <input class="userBtn" type="button" value="server 유저 정보" @click="getUserInfo">
+        <div>
+            {{username}}
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: 'Home',
     props: [],
     data: function() {
         return {
-            oauth_way: 0,
+            username: "",
+        }
+    },
+    methods: {
+        getUserInfo: function() {
+            axios.get(this.$serverUrl + '/user')
+                .then(r => {
+                    this.username = r.data
+                }).catch(e => this.username = "")
         }
     }
 }
 </script>
+
+<style scoped>
+    .userBtn {
+        margin-top: 20px;
+    }
+</style>
