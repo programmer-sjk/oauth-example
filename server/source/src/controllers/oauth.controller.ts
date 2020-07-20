8389c98334bbcd0d05ac8d94acbec6c7601e76fe
sjk5766 <wjdrnr6662@gmail.com>
2020-07-20 13:58:43 +0900

    코드 공통화

--- server/source/src/controllers/oauth.controller.ts
+++ server/source/src/controllers/oauth.controller.ts
@@ -20,16 +20,16 @@ class OauthController {
         this.router.get('/auth/kakao/callback', this.callback);
     }
 
-    private callback(req, res, next) {
-        const type = req.url.split('/')[2]
+    private callback(request: express.Request, response: express.Response) {
+        const type = request.url.split('/')[2]
         passport.authenticate(type, function (err, user) {
-            if (!user) { return res.redirect('http://localhost:3000'); }
-            req.logIn(user, function (err) { 
-                const tokenContent = { name: req.user!.displayName }
+            if (!user) { return response.redirect('http://localhost:3000'); }
+            request.logIn(user, function (err) { 
+                const tokenContent = { name: request.user!.displayName }
                 const token = jwt.sign(tokenContent, 'secret', { expiresIn: 30 });
-                res.redirect('http://localhost/home?token=' + token);            
+                response.redirect('http://localhost/home?token=' + token);            
             });
-        })(req, res);
+        })(request, response);
     }
 }
 
